665a2afa0a4ae07ec7ad51655704c0529ff5301f
achiever-or-loser <1379945771@qq.com>
2020-07-01 22:38:05 +0800

    elasticsearch7.6.1

---
+++ src/main/java/com/csc/elasticsearch/pojo/User.java
@@ -0,0 +1,31 @@
+package com.csc.elasticsearch.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+/**
+ * @description:
+ * @author: csc
+ * @create: 2020/5/26 16:03
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(indexName = "csc_index",type = "user")
+public class User {
+    @Id
+    @Field(analyzer = "ik_smart",type = FieldType.Integer)
+    private Integer id;
+
+    @Field(analyzer = "ik_smart",type = FieldType.Text)
+    private String name;
+
+    @Field(analyzer = "ik_smart",type = FieldType.Integer)
+    private Integer age;
+
+}
